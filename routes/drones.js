5d4ff0a0d472b80118b0bdbbff379195ce5e02db
Alejandro Rodriguez <aperturegir@gmail.com>
2023-08-08 16:27:13 +0100

    Iteracion 3 done

--- routes/drones.js
+++ routes/drones.js
@@ -1,21 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require("../models/Drone.model.js")
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  Drone.find({}, 'name propellers maxSpeed') 
+  .then((drones) => {
+    res.render('drones/list', {
+      drones: drones, 
+    });
+  })
+  .catch((error) => {
+    console.error('Error fetching drones:', error);
+   
+  });
 });
+
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render("drones/create-form.hbs")
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  try {
+    const { name, propellers, maxSpeed } = req.body;
+
+  
+    if (name === '' || propellers === '' || maxSpeed === '') {
+      return res.status(400).render('drones/create-form', {
+        errorMessage: 'All fields are required',
+        previousFormData: req.body 
+      });
+    }
+
+    if (name.length <= 3) {
+      return res.status(400).render('drones/create-form', {
+        errorMessage: 'Name must have more than 3 characters',
+        previousFormData: req.body
+      });
+    }
+
+    // Create a new drone using the provided data
+    const newDrone = new Drone({ name, propellers, maxSpeed });
+    await newDrone.save();
+
+    res.redirect('/drones'); 
+  } catch (error) {
+    console.error('Error creating drone:', error);
+  
+    res.status(500).render('drones/create-form', {
+      errorMessage: 'Error creating drone.'
+    });
+  }
+
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,36 @@
 // Iteration #1
+const mongoose = require("mongoose")
+const Drone = require("../models/Drone.model.js")
+
+
+
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-drones";
+
+  mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    // Create drones
+    return Drone.create(drones);
+  })
+  .then(createdDrones => {
+    console.log(`${createdDrones.length} drones have been created.`);
+
+    // Disconnect from the database
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log("Disconnected from the database");
+  })
+  .catch(err => {
+    console.error("Error:", err);
+    mongoose.disconnect();
+  });
